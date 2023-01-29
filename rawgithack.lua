5a98349edb4f5d9160194fa68a212c5d1678e929
Pavel Puchkin <i@neoascetic.me>
2023-01-29 15:44:04 +0200

    Implement backend

--- rawgithack.lua
+++ rawgithack.lua
@@ -1,38 +1,73 @@
-local json = require "cjson"
+local json = require("cjson")
+local http = require("resty.http")
+local cfg = require("config")
 
-local cfg = require "config"
-local url = 'https://api.cloudflare.com/client/v4/zones/' .. cfg.cf.zone .. '/purge_cache'
+local patrons = {}
 
 
-local function post(url, data, headers)
-   return ngx.location.capture(
-      '/proxy',
-      {
-         ctx = {headers = headers},
-         vars = {_url = url},
-         method = ngx.HTTP_POST,
-         body = data
-      })
+local function dump_file(path, data)
+   local file, err = io.open(path, "w")
+   if err then return ngx.log(ngx.WARN, "File dumping error: " .. err) end
+   file:write(data)
+   file:close()
 end
 
 
-local function error(desc, extended)
+local function load_file(path)
+   local file, err = io.open(path, "r")
+   if err then return ngx.log(ngx.WARN, "File reading error: " .. err) end
+   local data = file:read("*all")
+   file:close()
+   return data
+end
+
+
+local function refresh_patrons(_premature, cold_cache_path)
+   local headers = {['Authorization'] = 'Bearer ' .. cfg.patreon.token}
+   local next_page = table.concat({
+     'https://www.patreon.com/api/oauth2/v2/campaigns',
+     cfg.patreon.campaign,
+     'members?fields%5Bmember%5D=email,patron_status'}, '/')
+   local acc = {}
+   while next_page ~= json.null do
+     local res, err = http.new():request_uri(next_page, {headers=headers})
+     if res and res.status ~= 200 then err = res.body end
+     if err then return ngx.log(ngx.WARN, "Cannot refresh patrons list: " .. err) end
+     local result = json.decode(res.body)
+     for i = 1, #result['data'] do
+         local user = result['data'][i]['attributes']
+         if (user['patron_status'] == 'active_patron' or
+             user['patron_status'] == 'declined_patron') then
+            acc[user['email']] = true
+         end
+     end
+     next_page = result['meta']['pagination']['cursors']['next']
+   end 
+   patrons = acc
+   dump_file(cold_cache_path, json.encode(acc))
+end
+
+
+local function init()
+   local cold_cache_path = "/var/cache/nginx/rawgithack/patrons.json"
+   ngx.timer.every(60, refresh_patrons, cold_cache_path)
+   local cold_cache = load_file(cold_cache_path)
+   if cold_cache ~= nil then patrons = json.decode(cold_cache) end
+end
+
+
+local function error(desc)
    ngx.status = ngx.HTTP_BAD_REQUEST
-   if extended then desc = desc .. ': ' .. extended end
-   ngx.log(ngx.ERR, desc)
    ngx.say(json.encode({success = false, response = desc}))
    ngx.exit(ngx.status)
 end
 
 
-local function get_files()
-   local args, err = ngx.req.get_post_args()
-
-   if err == "truncated" then error("truncated request") end
-   if not args.files then error("wrong number of URLs") end
+local function validate_files(raw_files)
+   if not raw_files then error("wrong number of URLs") end
 
    local files, invalid_files = {}, {}
-   for l in args.files:gmatch('[^\r\n]+') do
+   for l in raw_files:gmatch('[^\r\n]+') do
       local url = l:gsub('^%s*(.*)%s*$', '%1') -- trailing whitespaces
       local valid = url:match('^https?://%w+cdn%.githack%.com')
       table.insert(valid and files or invalid_files, url)
@@ -51,12 +86,17 @@ local function cdn_purge(files)
       ['X-Auth-Email'] = cfg.cf.username,
       ['X-Auth-Key'] = cfg.cf.api_key
    }
-   local res = post(url, json.encode({files = files}), headers)
-   if res.status == 200 then
-      ngx.say(json.encode({success = true, response = 'cache was successfully invalidated!'}))
-   else
-      error("cdn response error", res.body)
+   local purge_url = 'https://api.cloudflare.com/client/v4/zones/' .. cfg.cf.zone .. '/purge_cache'
+   local params = {
+       method='POST',
+       headers=headers,
+       body=json.encode({files=files})}
+   local res = http.new():request_uri(purge_url, params)
+   if res.status ~= 200 then
+      ngx.log(ngx.ERR, "CDN response error: " .. res.body)
+      return false
    end
+   return true
 end
 
 
@@ -94,29 +134,21 @@ end
 
 
 local function purge_request()
-   local files = get_files()
+   local args, err = ngx.req.get_post_args()
+   if err == "truncated" then error("truncated request") end
+
+   if (not patrons[args.patron] and
+       args.patron ~= cfg.simsim) then error("unknown patron") end
+
+   local files = validate_files(args.files)
    ngx.log(ngx.WARN, "got a request to purge #" .. #files .. " files")
    local_purge(files) 
-   cdn_purge(files)
-end
-
-
-local function patch_proxy()
-   local req = ngx.req
-   for k,v in pairs(req.get_headers()) do
-      if k ~= 'content-length' then
-         req.clear_header(k)
-      end
-   end
-   if ngx.ctx.headers then
-      for k,v in pairs(ngx.ctx.headers) do
-         req.set_header(k, v)
-      end
-   end
+   if not cdn_purge(files) then error("CDN response error") end
+   ngx.say(json.encode({success = true, response = 'cache was successfully invalidated!'}))
 end
 
 
 return {
-   purge_request = purge_request,
-   patch_proxy = patch_proxy
+   init = init,
+   purge_request = purge_request
 }
