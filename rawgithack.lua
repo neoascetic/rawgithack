51c81a8624516923f518f04d8dd5db26d3ca8833
Pavel Puchkin <i@neoascetic.me>
2026-04-01 07:20:30 +0200

    Allow purge for everyone

--- rawgithack.lua
+++ rawgithack.lua
@@ -2,58 +2,6 @@ local json = require("cjson.safe")
 local http = require("resty.http")
 local cfg = require("config")
 
-local patrons = {}
-
-
-local function dump_file(path, data)
-   local file, err = io.open(path, "w")
-   if err then return ngx.log(ngx.WARN, "File dumping error: " .. err) end
-   file:write(data)
-   file:close()
-end
-
-
-local function load_file(path)
-   local file, err = io.open(path, "r")
-   if err then return ngx.log(ngx.WARN, "File reading error: " .. err) end
-   local data = file:read("*all")
-   file:close()
-   return data
-end
-
-
-local function refresh_patrons(_premature, cold_cache_path)
-   local headers = {['Authorization'] = 'Bearer ' .. cfg.patreon.token}
-   local next_page = table.concat({
-     'https://www.patreon.com/api/oauth2/v2/campaigns',
-     cfg.patreon.campaign,
-     'members?fields%5Bmember%5D=email,patron_status'}, '/')
-   local acc = {}
-   while next_page and next_page ~= json.null do
-     local res, err = http.new():request_uri(next_page, {headers=headers})
-     if res and res.status ~= 200 then err = res.body end
-     if err then return ngx.log(ngx.WARN, "Cannot refresh patrons list: " .. err) end
-     local result = json.decode(res.body)
-     for i = 1, #result['data'] do
-         local user = result['data'][i]['attributes']
-         if user['patron_status'] == 'active_patron' then
-            acc[user['email']:lower()] = true
-         end
-     end
-     next_page = (result['links'] or {})['next']
-   end 
-   patrons = acc
-   dump_file(cold_cache_path, json.encode(acc))
-end
-
-
-local function init()
-   local cold_cache_path = "/var/cache/nginx/rawgithack_patrons/patrons.json"
-   ngx.timer.every(60, refresh_patrons, cold_cache_path)
-   local cold_cache = load_file(cold_cache_path)
-   if cold_cache ~= nil then patrons = json.decode(cold_cache) end
-end
-
 
 local function error(desc)
    ngx.status = ngx.HTTP_BAD_REQUEST
@@ -117,10 +65,6 @@ local function purge_request()
    local args = json.decode(ngx.req.get_body_data())
    if not args then error("invalid request") end
 
-   if (type(args.patron) ~= 'string' or not patrons[args.patron:lower()] and args.patron ~= cfg.simsim)
-     then error("you are not our patron")
-   end
-
    local files = validate_files(args.files)
    ngx.log(ngx.WARN, "got a request to purge #" .. #files .. " files")
    if not cdn_purge(files) then error("CDN response error") end
@@ -129,6 +73,5 @@ end
 
 
 return {
-   init = init,
    purge_request = purge_request
 }
